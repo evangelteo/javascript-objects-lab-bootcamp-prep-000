2375b61390522d574b453afe7c553357019f1a9d
Evangel Teo <evangelteo@gmail.com>
2018-05-21 06:42:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj,key,value) {
 }
 
 function deleteFromObjectByKey(object,key){
-  var clone = object
+  var clone = Object.assign({},object)
   delete clone[key]
   return clone
 }
