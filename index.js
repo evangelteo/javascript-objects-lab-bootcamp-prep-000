2451769b07281f646fd63e0b30e1ad4d2d3e0b30
Evangel Teo <evangelteo@gmail.com>
2018-05-21 06:33:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,27 +4,11 @@ function updateObjectWithKeyAndValue(obj,key,value) {
   return Object.assign({},obj,key,value)
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(obj,key,value) {
+  return Object.assign(obj,key,value)
+}
 
-
-  describe('updateObjectWithKeyAndValue(object, key, value)', function() {
-    it('returns an object with the orignal key value pairs and the new key value pair', function() {
-      var obj = { prop: 1 }
-
-      expect(updateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
-        prop: 1,
-        prop2: 2
-      })
-    })
-
-    it('it does not modify the original object, but rather returns a clone with the new data', function() {
-      var obj = { prop: 1 }
-
-      updateObjectWithKeyAndValue(obj, 'prop2', 2)
-
-      expect(obj['prop2']).toBe(undefined)
-    })
-  })
-
+ 
   describe('destructivelyUpdateObjectWithKeyAndValue(object, key, value)', function() {
     it('updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object', function() {
       var obj = { prop: 1 }
